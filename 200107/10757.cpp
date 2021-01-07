36c0f75c29cbaebe2061a3cdf127e32b1976f38f
DESKTOP-P8RTASM\Cloud <gomdalp1701@gmail.com>
2021-01-07 22:14:43 +0900

    초기 커밋.

---
+++ 200107/10757.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string A, B;
+
+	cin >> A >> B;
+
+	int Asize, Bsize;
+	Asize = A.size();
+	Bsize = B.size();
+
+	if (Asize > Bsize)
+	{
+		//°Á pythonÀ¸·Î Çª¼À
+	}
+}
